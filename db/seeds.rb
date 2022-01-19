178f246c451994bb05bdc21a41990ad9c4e5886c
Andres Munoz <andremude@hotmail.com>
2022-01-19 16:59:08 -0500

    rails yelp mvp completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+restaurant_one = Restaurant.create(name: 'Maido', address:'Calle San Martin 399, Miraflores', phone_number: '3333333', category:'chinese')
+restaurant_two = Restaurant.create(name: 'Central Restaurante', address:'Av. Pedro de Osma 301 Lima, Barranco', phone_number: '4444444', category:'french')
+restaurant_three = Restaurant.create(name: 'Saha', address:'Calle Bolívar 164, Miraflores', phone_number: '1111111', category:'japanese')
+restaurant_four = Restaurant.create(name: 'La Mar', address:'Av. Mariscal La Mar 770', phone_number: '2222222', category:'italian')
+restaurant_five = Restaurant.create(name: 'Panchita', address:'Calle 2 de Mayo 298, Miraflores', phone_number: '5555555', category:'belgian')
+
+puts "Finished!"
